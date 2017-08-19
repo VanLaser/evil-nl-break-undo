07be9678e4aba2b50a3a87e633a429683fb464b3
Van Laser <gabi_laser@yahoo.com>
2017-08-20 00:17:50 +0300

    Make it more generic, use a modifiable regexp instead of "\n".

--- evil-nl-break-undo.el
+++ evil-nl-break-undo.el
@@ -2,6 +2,10 @@
 
 (require 'evil)
 
+(defvar evil-break-undo-regexp "\n"
+  "REGEXP used to break an evil undo sequence.
+The search takes place on the text range that is being changed.")
+
 (defun evil-nl-break-undo--maybe (beg end &optional len)
   "Local hook run before and after the buffer is changed.
 Checks if a newline was either removed, or inserted, and breaks
@@ -12,7 +16,7 @@ the current evil undo step."
 	     (< beg end)
 	     (save-excursion
 	       (save-match-data		;don't spoil user searches
-		 (goto-char beg) (search-forward "\n" end t))))
+		 (goto-char beg) (search-forward evil-break-undo-regexp end t))))
     (evil-end-undo-step)
     (evil-echo "Break undo")
     (evil-start-undo-step)))
