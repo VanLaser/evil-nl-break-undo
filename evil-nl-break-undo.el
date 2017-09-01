1d8ee43d2a07952e2919cd0a8d1d6a152559d41a
Van Laser <gabi_laser@yahoo.com>
2017-09-01 21:24:31 +0300

    Add license and lexical binding.

--- evil-nl-break-undo.el
+++ evil-nl-break-undo.el
@@ -1,4 +1,42 @@
-;;; Evil 'break undo when line changes' mode
+;;; Evil 'break undo when line changes' mode --- -*- lexical-binding: t; coding: utf-8 -*-
+
+;; Copyright (C) 2017 Gabriel Lazar
+
+;; Author: VanLaser <Gabriel.Lazar@com.utcluj.ro>
+;; URL: https://github.com/VanLaser/evil-nl-break-undo
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Simple minor mode that breaks evil's undo sequence when the buffer
+;; is changed over a line boundary.
+;;
+
+;;; Usage:
+;;
+;; Add the mode to the buffers you want (Evil should be already enabled there).
+;; For example:
+;;
+;; (add-hook 'text-mode-hook #'evil-nl-break-undo-mode)
+;; (add-hook 'prog-mode-hook #'evil-nl-break-undo-mode)
+
+;;; Code:
 
 (require 'evil)
 
